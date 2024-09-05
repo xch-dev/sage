da2594a26e9c6f52d2cc52a8960d6ff5de4be109
Rigidity <me@rigidnetwork.com>
2024-09-04 23:58:49 -0400

    Remove debug thing

--- src/pages/PeerList.tsx
+++ src/pages/PeerList.tsx
@@ -63,8 +63,6 @@ export default function NetworkList() {
         }}
       />
 
-      {peers?.length ?? 0}
-
       <ListContainer>
         {peers !== null && (
           <List sx={{ width: '100%' }} component={Paper} disablePadding>
