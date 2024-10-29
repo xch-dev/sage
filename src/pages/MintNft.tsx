2433d157eef02ec2483351fe91b572275ec6ea04
Rigidity <me@rigidnetwork.com>
2024-10-29 01:15:00 -0400

    Another

--- src/pages/MintNft.tsx
+++ src/pages/MintNft.tsx
@@ -1,3 +1,4 @@
+import ConfirmationDialog from '@/components/ConfirmationDialog';
 import Header from '@/components/Header';
 import { Button } from '@/components/ui/button';
 import {
@@ -24,7 +25,7 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import * as z from 'zod';
-import { commands, Error } from '../bindings';
+import { commands, Error, TransactionSummary } from '../bindings';
 import Container from '../components/Container';
 import ErrorDialog from '../components/ErrorDialog';
 import { useWalletState } from '../state';
@@ -37,6 +38,7 @@ export default function MintNft() {
 
   const [error, setError] = useState<Error | null>(null);
   const [pending, setPending] = useState(false);
+  const [summary, setSummary] = useState<TransactionSummary | null>(null);
 
   const formSchema = z.object({
     profile: z.string().min(1, 'Profile is required'),
@@ -52,47 +54,45 @@ export default function MintNft() {
     resolver: zodResolver(formSchema),
   });
 
-  console.log(form.getValues());
-
   const onSubmit = (values: z.infer<typeof formSchema>) => {
     setPending(true);
+
     commands
-      .bulkMintNfts(
-        {
-          fee: values.fee?.toString() || '0',
-          did_id: values.profile,
-          nft_mints: [
-            {
-              edition_number: null,
-              edition_total: null,
-              royalty_address: values.royaltyAddress || null,
-              royalty_percent: values.royaltyPercent,
-              data_uris: values.dataUris
-                .split(',')
-                .map((uri) => uri.trim())
-                .filter(Boolean),
-              metadata_uris: values.metadataUris
-                .split(',')
-                .map((uri) => uri.trim())
-                .filter(Boolean),
-              license_uris: (values.licenseUris ?? '')
-                .split(',')
-                .map((uri) => uri.trim())
-                .filter(Boolean),
-            },
-          ],
-        },
-        false,
-      )
+      .bulkMintNfts({
+        fee: values.fee?.toString() || '0',
+        did_id: values.profile,
+        nft_mints: [
+          {
+            edition_number: null,
+            edition_total: null,
+            royalty_address: values.royaltyAddress || null,
+            royalty_percent: values.royaltyPercent,
+            data_uris: values.dataUris
+              .split(',')
+              .map((uri) => uri.trim())
+              .filter(Boolean),
+            metadata_uris: values.metadataUris
+              .split(',')
+              .map((uri) => uri.trim())
+              .filter(Boolean),
+            license_uris: (values.licenseUris ?? '')
+              .split(',')
+              .map((uri) => uri.trim())
+              .filter(Boolean),
+          },
+        ],
+      })
       .then((result) => {
         if (result.status === 'error') {
           console.error(result.error);
           setError(result.error);
-          return;
+        } else {
+          setSummary(result.data.summary);
         }
-        navigate('/nfts');
       })
-      .finally(() => setPending(false));
+      .finally(() => {
+        setPending(false);
+      });
   };
 
   return (
@@ -272,6 +272,11 @@ export default function MintNft() {
       </Container>
 
       <ErrorDialog error={error} setError={setError} />
+      <ConfirmationDialog
+        summary={summary}
+        close={() => setSummary(null)}
+        onConfirm={() => navigate('/nfts')}
+      />
     </>
   );
 }
