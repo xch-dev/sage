22a6b033c92d33178e0f74984ba039c857ca3ac7
Don Kackman <dkackman@gmail.com>
2025-01-20 13:13:28 -0600

    https://github.com/xch-dev/sage/issues/275

--- src/components/TokenListView.tsx
+++ src/components/TokenListView.tsx
@@ -10,7 +10,7 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-
+import { t } from '@lingui/core/macro';
 interface TokenListViewProps {
   cats: Array<CatRecord & { balanceInUsd: number; sortValue: number }>;
   xchBalance: string;
@@ -25,65 +25,84 @@ export function TokenListView({
   getBalanceInUsd 
 }: TokenListViewProps) {
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead></TableHead>
-          <TableHead><Trans>Name</Trans></TableHead>
-          <TableHead><Trans>Symbol</Trans></TableHead>
-          <TableHead className="text-right"><Trans>Balance</Trans></TableHead>
-          <TableHead className="text-right"><Trans>Value (USD)</Trans></TableHead>
-          <TableHead className="text-right"><Trans>Price</Trans></TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell>
-            <img
-              alt={`XCH logo`}
-              className='h-6 w-6'
-              src='https://icons.dexie.space/xch.webp'
-            />
-          </TableCell>
-          <TableCell>Chia</TableCell>
-          <TableCell>XCH</TableCell>
-          <TableCell className="text-right">{toDecimal(xchBalance, xchDecimals)}</TableCell>
-          <TableCell className="text-right">
-            ${getBalanceInUsd('xch', toDecimal(xchBalance, xchDecimals))}
-          </TableCell>
-          <TableCell className="text-right">
-            ${Number(getBalanceInUsd('xch', '1')).toFixed(2)}
-          </TableCell>
-        </TableRow>
-        {cats.map((cat) => (
-          <TableRow key={cat.asset_id}>
+    <div role="region" aria-label={t`Token List`}>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead scope="col"></TableHead>
+            <TableHead scope="col"><Trans>Name</Trans></TableHead>
+            <TableHead scope="col"><Trans>Symbol</Trans></TableHead>
+            <TableHead scope="col" className="text-right"><Trans>Balance</Trans></TableHead>
+            <TableHead scope="col" className="text-right"><Trans>Value (USD)</Trans></TableHead>
+            <TableHead scope="col" className="text-right"><Trans>Price</Trans></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
             <TableCell>
-              {cat.icon_url && (
-                <img
-                  alt={`${cat.asset_id} logo`}
-                  className='h-6 w-6'
-                  src={cat.icon_url}
-                />
-              )}
+              <img
+                alt={t`XCH logo`}
+                aria-hidden="true"
+                className='h-6 w-6'
+                src='https://icons.dexie.space/xch.webp'
+              />
             </TableCell>
-            <TableCell className="font-medium">
-              <Link to={`/wallet/token/${cat.asset_id}`}>
-                {cat.name || <Trans>Unknown CAT</Trans>}
+            <TableCell>
+              <Link to="/wallet/token/xch" className="hover:underline">
+                Chia
               </Link>
             </TableCell>
-            <TableCell>{cat.ticker || '-'}</TableCell>
+            <TableCell>XCH</TableCell>
             <TableCell className="text-right">
-              {toDecimal(cat.balance, 3)}
+              {toDecimal(xchBalance, xchDecimals)}
             </TableCell>
-            <TableCell className="text-right">${cat.balanceInUsd.toFixed(2)}</TableCell>
             <TableCell className="text-right">
-              {Number(cat.balance) > 0
-                ? `$${(cat.balanceInUsd / Number(toDecimal(cat.balance, 3))).toFixed(2)}`
-                : '-'}
+              <span className="sr-only">USD Value: </span>
+              ${getBalanceInUsd('xch', toDecimal(xchBalance, xchDecimals))}
+            </TableCell>
+            <TableCell className="text-right">
+              <span className="sr-only">Price per token: </span>
+              ${Number(getBalanceInUsd('xch', '1')).toFixed(2)}
             </TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+          {cats.map((cat) => (
+            <TableRow key={cat.asset_id}>
+              <TableCell>
+                {cat.icon_url && (
+                  <img
+                    alt={t`Token logo`}
+                    aria-hidden="true"
+                    className='h-6 w-6'
+                    src={cat.icon_url}
+                  />
+                )}
+              </TableCell>
+              <TableCell>
+                <Link 
+                  to={`/wallet/token/${cat.asset_id}`}
+                  className="hover:underline"
+                >
+                  {cat.name || <Trans>Unknown CAT</Trans>}
+                </Link>
+              </TableCell>
+              <TableCell>{cat.ticker || '-'}</TableCell>
+              <TableCell className="text-right">
+                {toDecimal(cat.balance, 3)}
+              </TableCell>
+              <TableCell className="text-right">
+                <span className="sr-only">USD Value: </span>
+                ${cat.balanceInUsd.toFixed(2)}
+              </TableCell>
+              <TableCell className="text-right">
+                <span className="sr-only">Price per token: </span>
+                {Number(cat.balance) > 0
+                  ? `$${(cat.balanceInUsd / Number(toDecimal(cat.balance, 3))).toFixed(2)}`
+                  : '-'}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 } 
