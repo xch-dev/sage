8860ea0501c46ce50e0b4de690710b0aee2be2f0
Gabriel Inzirillo <gabriel@inzirillo.dev>
2025-01-25 21:36:36 +0100

    Enhance Pagination component with optional total and load more support

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -7,12 +7,15 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { t } from '@lingui/core/macro';
+import { Trans } from '@lingui/react/macro';
 import { Button } from './ui/button';
 import { ChevronRightIcon, ChevronLeftIcon } from '@radix-ui/react-icons';
 
 type PaginationProps = {
   page: number;
-  total: number;
+  total?: number;
+  canLoadMore?: boolean;
+  isLoading?: boolean;
   onPageChange: (page: number) => void;
   pageSize: number;
   onPageSizeChange: (pageSize: number) => void;
@@ -21,51 +24,69 @@ type PaginationProps = {
 export const Pagination = ({
   page,
   total,
+  canLoadMore,
+  isLoading,
   onPageChange,
   pageSize,
   onPageSizeChange,
 }: PaginationProps) => {
-  const totalPages = Math.max(1, Math.ceil(total / pageSize));
+  const totalPages = total
+    ? Math.max(1, Math.ceil(total / pageSize))
+    : undefined;
   const isFirstPage = page === 1;
-  const isLastPage = page === totalPages;
+  const isLastPage = totalPages
+    ? page === totalPages
+    : !canLoadMore || isLoading;
 
   return (
-    <div className='flex justify-between flex-wrap gap-2'>
-      <div className='flex items-center justify-start gap-2'>
+    <div className='flex justify-between gap-2'>
+      <div className='flex items-center justify-start gap-3'>
         <Button
           size='icon'
           variant='outline'
-          onClick={() => onPageChange(Math.max(1, page - 1))}
+          onClick={() => {
+            if (isFirstPage) return;
+            onPageChange(Math.max(1, page - 1));
+          }}
           disabled={isFirstPage}
           title={t`Previous page`}
         >
           <ChevronLeftIcon className='h-4 w-4' />
         </Button>
 
-        <Select
-          onValueChange={(value) => onPageChange(parseInt(value))}
-          defaultValue={page.toString()}
-          value={page.toString()}
-          disabled={totalPages === 1}
-        >
-          <SelectTrigger className='w-min text-sm' title={t`Page`}>
-            <SelectValue placeholder={t`Page`}>
-              {page}/{totalPages}
-            </SelectValue>
-          </SelectTrigger>
-          <SelectContent>
-            {Array.from({ length: totalPages }, (_, i) => (
-              <SelectItem key={i} value={(i + 1).toString()}>
-                {i + 1}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
+        {totalPages ? (
+          <Select
+            onValueChange={(value) => onPageChange(parseInt(value))}
+            defaultValue={page.toString()}
+            value={page.toString()}
+            disabled={totalPages === 1}
+          >
+            <SelectTrigger className='w-min text-sm' title={t`Page`}>
+              <SelectValue placeholder={t`Page`}>
+                {page} / {totalPages}
+              </SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              {Array.from({ length: totalPages }, (_, i) => (
+                <SelectItem key={i} value={(i + 1).toString()}>
+                  {i + 1}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        ) : (
+          <div className='text-sm text-muted-foreground font-medium  text-center'>
+            <Trans>Page {page}</Trans>
+          </div>
+        )}
 
         <Button
           size='icon'
           variant='outline'
-          onClick={() => onPageChange(Math.min(totalPages, page + 1))}
+          onClick={() => {
+            if (isLastPage) return;
+            onPageChange(Math.min(totalPages ?? Infinity, page + 1));
+          }}
           disabled={isLastPage}
           title={t`Next page`}
         >
