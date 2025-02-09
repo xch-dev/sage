67df74231f83794d8ba3e356c6207a071359cb16
Don Kackman <dkackman@gmail.com>
2025-02-09 13:48:35 -0600

    make sure selected drop down item is selected on enter keypress

--- src/components/selectors/DropdownSelector.tsx
+++ src/components/selectors/DropdownSelector.tsx
@@ -70,21 +70,21 @@ export function DropdownSelector<T>({
           break;
         case 'Enter':
           event.preventDefault();
-          if (document.activeElement === inputRef.current) {
-            // If Enter is pressed while input has focus, select first non-disabled item
+          if (selectedIndex >= 0 && selectedIndex < loadedItems.length) {
+            // If an item is selected, use that
+            const item = loadedItems[selectedIndex];
+            if (!isDisabled?.(item)) {
+              onSelect(item);
+              setIsOpen(false);
+              setSelectedIndex(-1);
+            }
+          } else {
+            // If no item is selected, use the first non-disabled item
             const firstValidIndex = loadedItems.findIndex(
               (item) => !isDisabled?.(item),
             );
             if (firstValidIndex >= 0) {
               onSelect(loadedItems[firstValidIndex]);
-              setIsOpen(false);
-              setSelectedIndex(-1);
-            }
-          } else if (selectedIndex >= 0 && selectedIndex < loadedItems.length) {
-            // If Enter is pressed while an item is selected
-            const item = loadedItems[selectedIndex];
-            if (!isDisabled?.(item)) {
-              onSelect(item);
               setIsOpen(false);
               setSelectedIndex(-1);
             }
