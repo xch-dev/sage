1f0b75cc8a942991625b6727505bf0ce33bd9cbd
Don Kackman <dkackman@gmail.com>
2025-02-14 12:41:08 -0600

    check that a pasted value can be converted to a number before allowing the paste. this prevents masked values from accepting ambiguous input - i.e. 0,0001 is masked to 1

--- src/components/ui/masked-input.tsx
+++ src/components/ui/masked-input.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Input, InputProps } from './input';
 import { NumericFormat, NumericFormatProps } from 'react-number-format';
+import { toast } from 'react-toastify';
+import { t } from '@lingui/core/macro';
 
 interface MaskedInputProps extends NumericFormatProps<InputProps> {
   inputRef?: React.Ref<HTMLInputElement>;
@@ -15,10 +17,43 @@ const MaskedInput = React.forwardRef<HTMLInputElement, MaskedInputProps>(
       displayType='input'
       type={type}
       value={value}
+      onPaste={(e) => {
+        const pastedText = e.clipboardData.getData('text');
+        if (!isLocaleNumber(pastedText)) {
+          e.preventDefault();
+          toast.error(t`Invalid number ${pastedText}`);
+          return;
+        }
+      }}
       {...props}
     />
   ),
 );
+
+function isLocaleNumber(stringNumber: string, locale?: string): boolean {
+  try {
+    // Use navigator.language as fallback if locale is not provided
+    const userLocale = locale || navigator.language;
+
+    // Get the decimal separator for this locale
+    const decimalSeparator = Intl.NumberFormat(userLocale)
+      .format(1.1)
+      .replace(/\p{Number}/gu, '');
+
+    // convert decimal separator to period
+    const normalizedNumber = stringNumber.replace(
+      new RegExp(`\\${decimalSeparator}`),
+      '.',
+    );
+
+    // Check if it's a valid number and not NaN
+    const parsedNumber = Number(normalizedNumber);
+    return !isNaN(parsedNumber) && isFinite(parsedNumber);
+  } catch (error) {
+    // Return false if there's any error in the parsing process
+    return false;
+  }
+}
 
 MaskedInput.displayName = 'MaskedInput';
 
