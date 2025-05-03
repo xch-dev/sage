fa1c67b3803b49c33fe94e88475f3ea58cb0314e
Rigidity <me@rigidnetwork.com>
2025-05-03 18:28:55 -0400

    DID normalization action

--- crates/sage-wallet/src/wallet/dids.rs
+++ crates/sage-wallet/src/wallet/dids.rs
@@ -1,16 +1,9 @@
-use chia::{
-    clvm_utils::tree_hash_atom,
-    protocol::{Bytes32, Coin, CoinSpend},
-    puzzles::{singleton::SingletonArgs, Proof},
-};
-use chia_wallet_sdk::{
-    driver::{Did, HashedPtr, SpendContext, StandardLayer},
-    types::Conditions,
-};
+use chia::protocol::{Bytes32, CoinSpend};
+use chia_wallet_sdk::driver::{Did, HashedPtr};
 
 use crate::WalletError;
 
-use super::{CreateDidAction, Id, SpendAction, TransferDidAction, Wallet};
+use super::{CreateDidAction, Id, NormalizeDidAction, SpendAction, TransferDidAction, Wallet};
 
 impl Wallet {
     pub async fn create_did(
@@ -60,105 +53,19 @@ impl Wallet {
         &self,
         did_ids: Vec<Bytes32>,
         fee: u64,
-        hardened: bool,
-        reuse: bool,
     ) -> Result<Vec<CoinSpend>, WalletError> {
-        if did_ids.is_empty() {
-            return Err(WalletError::EmptyBulkTransfer);
-        }
-
-        let mut dids = Vec::new();
-
-        for did_id in did_ids {
-            let Some(did) = self.db.spendable_did(did_id).await? else {
-                return Err(WalletError::MissingDid(did_id));
-            };
-
-            dids.push(did);
-        }
-
-        let coins = if fee > 0 {
-            self.select_p2_coins(fee as u128).await?
-        } else {
-            Vec::new()
-        };
-        let selected: u128 = coins.iter().map(|coin| coin.amount as u128).sum();
-
-        let change: u64 = (selected - fee as u128)
-            .try_into()
-            .expect("change amount overflow");
-
-        let p2_puzzle_hash = self.p2_puzzle_hash(hardened, reuse).await?;
-
-        let mut ctx = SpendContext::new();
-
-        let did_coin_ids = dids
-            .iter()
-            .map(|did| did.coin.coin_id())
-            .collect::<Vec<_>>();
-
-        for (i, did) in dids.into_iter().enumerate() {
-            let did_metadata_ptr = ctx.alloc(&did.info.metadata)?;
-            let did = did.with_metadata(HashedPtr::from_ptr(&ctx, did_metadata_ptr));
-
-            let synthetic_key = self.db.synthetic_key(did.info.p2_puzzle_hash).await?;
-            let p2 = StandardLayer::new(synthetic_key);
-
-            let conditions = if did_coin_ids.len() == 1 {
-                Conditions::new()
-            } else {
-                Conditions::new().assert_concurrent_spend(
-                    did_coin_ids[if i == 0 {
-                        did_coin_ids.len() - 1
-                    } else {
-                        i - 1
-                    }],
-                )
-            };
-
-            let mut new_info = did.info;
-            new_info.recovery_list_hash = Some(Bytes32::from(tree_hash_atom(&[])));
-            let new_inner_puzzle_hash = new_info.inner_puzzle_hash();
-
-            let memos = ctx.hint(did.info.p2_puzzle_hash)?;
-
-            did.spend_with(
-                &mut ctx,
-                &p2,
-                Conditions::new().create_coin(
-                    new_inner_puzzle_hash.into(),
-                    did.coin.amount,
-                    Some(memos),
-                ),
-            )?;
-
-            let did = Did {
-                coin: Coin::new(
-                    did.coin.coin_id(),
-                    SingletonArgs::curry_tree_hash(new_info.launcher_id, new_inner_puzzle_hash)
-                        .into(),
-                    did.coin.amount,
-                ),
-                proof: Proof::Lineage(did.child_lineage_proof()),
-                info: new_info,
-            };
-
-            let _did = did.update(&mut ctx, &p2, conditions)?;
-        }
-
-        if fee > 0 {
-            let mut conditions = Conditions::new()
-                .assert_concurrent_spend(did_coin_ids[0])
-                .reserve_fee(fee);
-
-            if change > 0 {
-                conditions = conditions.create_coin(p2_puzzle_hash, change, None);
-            }
-
-            self.spend_p2_coins(&mut ctx, coins, conditions).await?;
-        }
-
-        Ok(ctx.take())
+        Ok(self
+            .transact(
+                did_ids
+                    .into_iter()
+                    .map(|did_id| {
+                        SpendAction::NormalizeDid(NormalizeDidAction::new(Id::Existing(did_id)))
+                    })
+                    .collect(),
+                fee,
+            )
+            .await?
+            .coin_spends)
     }
 }
 
--- crates/sage-wallet/src/wallet/transaction/action/normalize_did.rs
+++ crates/sage-wallet/src/wallet/transaction/action/normalize_did.rs
@@ -1,12 +1,64 @@
-use crate::{Action, Id, Summary};
+use chia::{
+    clvm_utils::tree_hash_atom,
+    protocol::{Bytes32, Coin},
+    puzzles::{singleton::SingletonArgs, Proof},
+};
+use chia_wallet_sdk::{driver::Did, types::Conditions};
+
+use crate::{Action, Id, Lineation, Summary, WalletError};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct NormalizeDidAction {
     pub did_id: Id,
 }
 
+impl NormalizeDidAction {
+    pub fn new(did_id: Id) -> Self {
+        Self { did_id }
+    }
+}
+
 impl Action for NormalizeDidAction {
     fn summarize(&self, summary: &mut Summary, _index: usize) {
         summary.spent_dids.insert(self.did_id);
+    }
+
+    fn lineate(&self, lineation: &mut Lineation<'_>, _index: usize) -> Result<(), WalletError> {
+        let did = lineation.dids[&self.did_id];
+
+        let p2 = lineation
+            .p2
+            .get(&did.info.p2_puzzle_hash)
+            .ok_or(WalletError::MissingDerivation(did.info.p2_puzzle_hash))?;
+
+        let mut new_info = did.info;
+        new_info.recovery_list_hash = Some(Bytes32::from(tree_hash_atom(&[])));
+        let new_inner_puzzle_hash = new_info.inner_puzzle_hash();
+
+        let memos = lineation.ctx.hint(did.info.p2_puzzle_hash)?;
+
+        did.spend_with(
+            lineation.ctx,
+            p2,
+            Conditions::new().create_coin(
+                new_inner_puzzle_hash.into(),
+                did.coin.amount,
+                Some(memos),
+            ),
+        )?;
+
+        let did = Did {
+            coin: Coin::new(
+                did.coin.coin_id(),
+                SingletonArgs::curry_tree_hash(new_info.launcher_id, new_inner_puzzle_hash).into(),
+                did.coin.amount,
+            ),
+            proof: Proof::Lineage(did.child_lineage_proof()),
+            info: new_info,
+        };
+
+        lineation.dids[&self.did_id] = did.update(lineation.ctx, p2, Conditions::new())?;
+
+        Ok(())
     }
 }
