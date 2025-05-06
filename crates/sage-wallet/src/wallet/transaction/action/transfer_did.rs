b31633fb1e4e1e58b7b4f1e828f0e9b97f260fc2
Rigidity <me@rigidnetwork.com>
2025-05-06 15:14:44 -0400

    Add tests for transferring DIDs

--- crates/sage-wallet/src/wallet/transaction/action/transfer_did.rs
+++ crates/sage-wallet/src/wallet/transaction/action/transfer_did.rs
@@ -39,3 +39,79 @@ impl Action for TransferDidAction {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{SpendAction, TestWallet};
+
+    use test_log::test;
+
+    #[test(tokio::test)]
+    async fn test_action_transfer_did() -> anyhow::Result<()> {
+        let mut alice = TestWallet::new(1).await?;
+        let mut bob = alice.next(0).await?;
+
+        let (coin_spends, did) = alice.wallet.create_did(0).await?;
+        let did_id = did.info.launcher_id;
+        alice.transact(coin_spends).await?;
+        alice.wait_for_coins().await;
+
+        let coin_spends = alice
+            .wallet
+            .transact(vec![SpendAction::transfer_did(did_id, bob.puzzle_hash)], 0)
+            .await?
+            .coin_spends;
+
+        assert_eq!(coin_spends.len(), 1);
+
+        alice.transact(coin_spends).await?;
+        alice.wait_for_coins().await;
+        bob.wait_for_puzzles().await;
+
+        assert!(alice.wallet.db.spendable_did(did_id).await?.is_none());
+        assert!(bob.wallet.db.spendable_did(did_id).await?.is_some());
+
+        Ok(())
+    }
+
+    #[test(tokio::test)]
+    async fn test_action_transfer_multiple_dids() -> anyhow::Result<()> {
+        let mut alice = TestWallet::new(2).await?;
+        let mut bob = alice.next(0).await?;
+
+        let (coin_spends, did) = alice.wallet.create_did(0).await?;
+        let did_one = did.info.launcher_id;
+        alice.transact(coin_spends).await?;
+        alice.wait_for_coins().await;
+
+        let (coin_spends, did) = alice.wallet.create_did(0).await?;
+        let did_two = did.info.launcher_id;
+        alice.transact(coin_spends).await?;
+        alice.wait_for_coins().await;
+
+        let coin_spends = alice
+            .wallet
+            .transact(
+                vec![
+                    SpendAction::transfer_did(did_one, bob.puzzle_hash),
+                    SpendAction::transfer_did(did_two, bob.puzzle_hash),
+                ],
+                0,
+            )
+            .await?
+            .coin_spends;
+
+        assert_eq!(coin_spends.len(), 2);
+
+        alice.transact(coin_spends).await?;
+        alice.wait_for_coins().await;
+        bob.wait_for_puzzles().await;
+
+        assert!(alice.wallet.db.spendable_did(did_one).await?.is_none());
+        assert!(alice.wallet.db.spendable_did(did_two).await?.is_none());
+        assert!(bob.wallet.db.spendable_did(did_one).await?.is_some());
+        assert!(bob.wallet.db.spendable_did(did_two).await?.is_some());
+
+        Ok(())
+    }
+}
