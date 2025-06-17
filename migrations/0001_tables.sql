1155f37184acc28e8b40d860d44d06cc62443668
Rigidity <me@rigidnetwork.com>
2025-06-16 20:32:51 -0400

    Updates

--- migrations/0001_tables.sql
+++ migrations/0001_tables.sql
@@ -7,13 +7,6 @@ CREATE TABLE future_did_names (
     name TEXT NOT NULL
 );
 
-CREATE TABLE derivations (
-  id INTEGER PRIMARY KEY,
-  p2_puzzle_hash BLOB NOT NULL UNIQUE,
-  `index` INTEGER NOT NULL,
-  hardened BOOLEAN NOT NULL,
-  synthetic_key BLOB NOT NULL
-);
 
 CREATE TABLE rust_migrations (
   version INTEGER PRIMARY KEY
@@ -24,20 +17,25 @@ CREATE TABLE rust_migrations (
   - all BOOLEAN columns are named is_<name>
   - all foreign keys are specified with FOREIGN KEY (and indexed)
   - all UNIX timestamps are INTEGER and named <name>_timestamp
-  - except for blocks, derivations and rust_migrations, all tables have a surrogate primary key
+  - except for blocks and rust_migrations, all tables have a surrogate primary key
   - all natural keys are specified as UNIQUE (which also creates an auto-index)
 */
-CREATE TABLE offers (
-  id INTEGER PRIMARY KEY,
-  hash BLOB NOT NULL UNIQUE,
-  encoded_offer TEXT NOT NULL,
-  fee BLOB NOT NULL,
-  status INTEGER NOT NULL,
-  expiration_height INTEGER,
-  expiration_timestamp INTEGER,
-  inserted_timestamp INTEGER NOT NULL
-);
 
+/*
+ * A single table that represents all kinds of supported assets on the Chia blockchain:
+ * XCH = 0
+ * CAT = 1
+ * NFT = 2
+ * DID = 3
+ * Option = 4
+ *
+ * The hash represents the asset's unique on-chain identifier (asset id or launcher id).
+ * Everything else is for display purposes only
+ *
+ * Note: For CATs, there isn't a defined created or spent height for the whole asset class,
+ * but for singletons it's possible to no longer own the asset, but still reference it in
+ * transaction history or offers.
+ */
 CREATE TABLE assets (
   id INTEGER PRIMARY KEY,
   hash BLOB NOT NULL UNIQUE,
@@ -50,6 +48,113 @@ CREATE TABLE assets (
   created_height INTEGER
 );
 
+/*
+ * This isn't a comprehensive history of the blockchain, but keeps track of blocks that
+ * have been synced. It's primarily used for identifying the timestamp in which coins
+ * were created or spent.
+ *
+ * However, its secondary use is for performing a rollback if the header hash of a
+ * previous block is changed (a fork).
+ */
+CREATE TABLE blocks (
+  height INTEGER PRIMARY KEY,
+  header_hash BLOB,
+  timestamp INTEGER
+);
+
+/*
+ * A table of all p2 puzzle hashes that belong to the wallet, from kinds such as:
+ * P2_DELEGATED_PUZZLE_OR_HIDDEN_PUZZLE = 0
+ * P2_DELEGATED_PUZZLE = 1
+ * CLAWBACK = 2
+ * OPTION_UNDERLYING = 3
+ *
+ * However, outer puzzles such as the CAT or revocation layer are stored elsewhere.
+ */
+CREATE TABLE p2_puzzles (
+  id INTEGER PRIMARY KEY,
+  hash BLOB NOT NULL UNIQUE,
+  kind INTEGER NOT NULL
+);
+
+/*
+ * A table of all synthetic keys that pertain to p2 puzzles.
+ * This is specifically for P2_DELEGATED_PUZZLE_OR_HIDDEN_PUZZLE and P2_DELEGATED_PUZZLE.
+ * The id is the derivation index of the key.
+ */
+CREATE TABLE public_keys (
+  id INTEGER PRIMARY KEY,
+  p2_puzzle_id INTEGER NOT NULL,
+  hardened BOOLEAN NOT NULL,
+  synthetic BOOLEAN NOT NULL,
+  key BLOB NOT NULL,
+  FOREIGN KEY (p2_puzzle_id) REFERENCES p2_puzzles(id) ON DELETE CASCADE
+);
+
+CREATE TABLE clawbacks (
+  id INTEGER PRIMARY KEY,
+  p2_puzzle_id INTEGER NOT NULL,
+  sender_puzzle_hash BLOB NOT NULL,
+  receiver_puzzle_hash BLOB NOT NULL,
+  seconds INTEGER NOT NULL,
+  FOREIGN KEY (p2_puzzle_id) REFERENCES p2_puzzles(id) ON DELETE CASCADE
+);
+
+CREATE TABLE p2_options (
+  id INTEGER PRIMARY KEY,
+  p2_puzzle_id INTEGER NOT NULL,
+  hash BLOB NOT NULL,
+  FOREIGN KEY (p2_puzzle_id) REFERENCES p2_puzzles(id) ON DELETE CASCADE
+);
+
+/*
+ * This is all coins which have been synced from the blockchain, created in a transaction,
+ * or spent in a transaction.
+ *
+ * When a coin is discovered, and hasn't been synced yet, it's added to this table without
+ * an asset_id. This will put it into a queue for further processing, in which case we
+ * will lookup the coin on-chain and discover the asset.
+ *
+ * The hint is for identifying 
+ */
+CREATE TABLE coins (
+  id INTEGER PRIMARY KEY,
+  asset_id INTEGER,
+  hash BLOB NOT NULL UNIQUE,
+  parent_coin_id BLOB NOT NULL,
+  puzzle_hash BLOB NOT NULL,
+  amount BLOB NOT NULL,
+  is_synced BOOLEAN NOT NULL,
+  p2_puzzle_id INTEGER,
+  memos BLOB,
+  created_height INTEGER,
+  spent_height INTEGER,
+  FOREIGN KEY (asset_id) REFERENCES assets(id) ON DELETE CASCADE,
+  FOREIGN KEY (p2_puzzle_id) REFERENCES p2_puzzles(id) ON DELETE SET NULL,
+  FOREIGN KEY (created_height) REFERENCES blocks(height) ON DELETE CASCADE,
+  FOREIGN KEY (spent_height) REFERENCES blocks(height) ON DELETE SET NULL
+);
+
+CREATE TABLE lineage_proofs (
+  id INTEGER PRIMARY KEY,
+  coin_id INTEGER NOT NULL UNIQUE,
+  parent_parent_coin_id BLOB NOT NULL,
+  parent_inner_puzzle_hash BLOB NOT NULL,
+  parent_amount BLOB NOT NULL,
+  FOREIGN KEY (coin_id) REFERENCES coins(id) ON DELETE CASCADE
+);
+
+CREATE TABLE offers (
+  id INTEGER PRIMARY KEY,
+  hash BLOB NOT NULL UNIQUE,
+  encoded_offer TEXT NOT NULL,
+  fee BLOB NOT NULL,
+  status INTEGER NOT NULL,
+  expiration_height INTEGER,
+  expiration_timestamp INTEGER,
+  inserted_timestamp INTEGER NOT NULL
+);
+
 CREATE TABLE offer_assets (
   id INTEGER PRIMARY KEY,
   offer_id INTEGER NOT NULL,
@@ -60,36 +165,6 @@ CREATE TABLE offer_assets (
   FOREIGN KEY (offer_id) REFERENCES offers(id) ON DELETE CASCADE,
   FOREIGN KEY (asset_id) REFERENCES assets(id) ON DELETE CASCADE,
   UNIQUE(offer_id, asset_id)
-);
-
-CREATE TABLE blocks (
-  height INTEGER PRIMARY KEY,
-  is_peak BOOLEAN NOT NULL DEFAULT FALSE,
-  header_hash BLOB,
-  timestamp INTEGER
-);
-
-CREATE TABLE coins (
-  id INTEGER PRIMARY KEY,
-  asset_id INTEGER NOT NULL,
-  hash BLOB NOT NULL UNIQUE,
-  parent_coin_id BLOB NOT NULL,
-  puzzle_hash BLOB NOT NULL,
-  amount BLOB NOT NULL,
-  is_synced BOOLEAN NOT NULL,
-  hint BLOB,
-  created_height INTEGER,
-  FOREIGN KEY (asset_id) REFERENCES assets(id) ON DELETE CASCADE,
-  FOREIGN KEY (created_height) REFERENCES blocks(height) ON DELETE SET NULL
-);
-
-CREATE TABLE lineage_proofs (
-  id INTEGER PRIMARY KEY,
-  coin_id INTEGER NOT NULL UNIQUE,
-  parent_parent_coin_id BLOB NOT NULL,
-  parent_inner_puzzle_hash BLOB NOT NULL,
-  parent_amount BLOB NOT NULL,
-  FOREIGN KEY (coin_id) REFERENCES coins(id) ON DELETE CASCADE
 );
 
 CREATE TABLE transactions (
@@ -165,11 +240,10 @@ CREATE TABLE nft_data (
   FOREIGN KEY (nft_id) REFERENCES nfts(id) ON DELETE CASCADE
 );
 
-CREATE TABLE tokens (
+CREATE TABLE cats (
   id INTEGER PRIMARY KEY,
   asset_id INTEGER NOT NULL UNIQUE,
   ticker TEXT,
-  is_xch BOOLEAN GENERATED ALWAYS AS (asset_id = 0) STORED,
   FOREIGN KEY (asset_id) REFERENCES assets(id) ON DELETE CASCADE
 );
 
@@ -185,8 +259,8 @@ CREATE TABLE dids (
 
 CREATE TABLE offer_coins (
   id INTEGER PRIMARY KEY,
-  offer_id INTEGER,
-  coin_id INTEGER UNIQUE,
+  offer_id INTEGER NOT NULL,
+  coin_id INTEGER NOT NULL,
   FOREIGN KEY (offer_id) REFERENCES offers(id) ON DELETE CASCADE
   FOREIGN KEY (coin_id) REFERENCES coins(id) ON DELETE CASCADE
   UNIQUE(offer_id, coin_id)
--- migrations/0002_seed_data.sql
+++ migrations/0002_seed_data.sql
@@ -1,5 +1,2 @@
 INSERT INTO assets (id, kind, hash, name, description, icon_url, is_visible, is_pending, created_height) 
 VALUES (0, 0, '0x0000000000000000000000000000000000000000000000000000000000000000', 'Chia', 'The chia token', 'https://icons.dexie.space/xch.webp', TRUE, FALSE, 0);
-
-INSERT INTO tokens (id, asset_id, ticker) 
-VALUES (0, 0, 'XCH');
