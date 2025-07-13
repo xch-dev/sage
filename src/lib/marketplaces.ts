f6d5f620c350783d22f029ea85f544b2eb309ae1
Don Kackman <dkackman@gmail.com>
2025-07-13 09:51:49 -0500

    disable eslint for unused var on this line

--- src/lib/marketplaces.ts
+++ src/lib/marketplaces.ts
@@ -23,12 +23,12 @@ export const marketplaces: MarketplaceConfig[] = [
     logo: 'https://raw.githubusercontent.com/dexie-space/dexie-kit/refs/heads/main/svg/duck.svg',
     qrCodeLogo:
       'https://raw.githubusercontent.com/dexie-space/dexie-kit/refs/heads/main/svg/duck.svg',
-    isSupported: (offer: OfferSummary | OfferState, isSplitting: boolean) => {
+    isSupported: (offer: OfferSummary | OfferState, _: boolean) => { //eslint-disable-line
       if ('taker' in offer) {
         return isDexieSupportedForSummary(offer);
       }
 
-      return isDexieSupported(offer, isSplitting);
+      return isDexieSupported(offer);
     },
     isOnMarketplace: (_, offerId, isTestnet) =>
       offerIsOnDexie(offerId, isTestnet),
