bca088c6abd9f7215b0a420fdca043a1576aca79
Don Kackman <dkackman@gmail.com>
2025-07-29 12:18:36 -0500

    lint

--- crates/sage-database/src/tables/assets/nft.rs
+++ crates/sage-database/src/tables/assets/nft.rs
@@ -301,8 +301,7 @@ impl Database {
         let total_count = query!(
             "SELECT COUNT(DISTINCT minter_hash) AS total_count 
             FROM owned_nfts 
-            WHERE minter_hash IS NOT NULL
-            "
+            WHERE minter_hash IS NOT NULL"
         )
         .fetch_one(&self.pool)
         .await?
