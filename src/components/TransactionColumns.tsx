f3ebd5c676d756cc1c5dd81c761940a0cce350be
Don Kackman <dkackman@gmail.com>
2025-08-30 09:53:08 -0500

    remove comment

--- src/components/TransactionColumns.tsx
+++ src/components/TransactionColumns.tsx
@@ -82,7 +82,6 @@ export const columns: ColumnDef<FlattenedTransaction>[] = [
                 icon_url: row.original.iconUrl ?? null,
                 kind: row.original.type,
                 revocation_address: null,
-                // TODO: Use Asset here and use the actual revocation address
               }}
               size='sm'
             />
